db93f9232adda77eedbf9b05a9dbb5de22e199f6
Abdul Mumit <mamumit07@gmail.com>
2017-10-15 17:31:29 +0200

    Show comic more often

--- cyanide-random-happiness.plugin.zsh
+++ cyanide-random-happiness.plugin.zsh
@@ -4,7 +4,7 @@ if [ "$TERM_PROGRAM" = "iTerm.app" ] && [ "$TMUX" = "" ]; then
   ! [ -d $CRH_DIR ] && mkdir $CRH_DIR
   ! [ -f $CRH_DIR/imgcat ] && curl https://www.iterm2.com/utilities/imgcat 2>/dev/null >$CRH_DIR/imgcat && chmod +x $CRH_DIR/imgcat
 
-  if [ "$(($RANDOM * `date +%S` % 11))" -eq "0" ]; then
+  if [ "$(($RANDOM * `date +%S` % 5))" -eq "0" ]; then
     curl http://files.explosm.net/rcg/`curl 'http://explosm.net/rcg/view/' 2>/dev/null| egrep -o '<img[^>]*>' | egrep -o '[^/]*\.png'` 2>/dev/null | $HOME/.cyanide-random-hapiness/imgcat /dev/stdin
   fi
 fi
